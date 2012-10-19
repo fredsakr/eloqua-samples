8557f4ac33e319e95cd2b4d9bb7c22537ecf124d
Fred Sakr <fred.sakr@eloqua.com>
2012-10-19 16:17:33 -0400

    initial import of contact helper project

---
+++ contact-helper/ContactSample/ContactHelper.cs
@@ -0,0 +1,92 @@
+﻿using System.Net;
+using ContactSample.Models;
+using RestSharp;
+
+namespace ContactSample
+{
+    public class ContactHelper
+    {
+        #region properties
+
+        private readonly RestSharp.RestClient _client;
+
+        #endregion
+
+        #region constructors
+
+        public ContactHelper(string site, string user, string password, string baseUrl)
+        {
+            _client = new RestClient(baseUrl)
+                          {
+                              Authenticator = new HttpBasicAuthenticator(site + "\\" + user, password)
+                          };
+        }
+
+        #endregion
+
+        #region methods
+
+        public Contact GetContact(int id)
+        {
+            var request = new RestRequest(Method.GET)
+                              {
+                                  Resource = "/data/contact/" + id
+                              };
+
+            var response = _client.Execute<Contact>(request);
+            return response.Data;
+        }
+
+        public RequestObjectList<Contact> SearchContacts(string searchTerm, int page, int pageSize)
+        {
+            var request = new RestRequest(Method.GET)
+                              {
+                                  Resource =
+                                      string.Format("/data/contacts?depth=complete&search={0}&page={1}&count={2}",
+                                                    searchTerm, page, pageSize)
+                              };
+
+            var response = _client.Execute<RequestObjectList<Contact>>(request);
+            return response.Data;
+        } 
+
+        public Contact CreateContact(Contact contact)
+        {
+            var request = new RestRequest(Method.POST)
+                              {
+                                  Resource = "/data/contact/",
+                                  RequestFormat = DataFormat.Json
+                              };
+            request.AddBody(contact);
+
+            var response = _client.Execute<Contact>(request);
+            return response.Data;
+        }
+
+        public Contact UpdateContact(Contact contact)
+        {
+            var request = new RestRequest(Method.PUT)
+                              {
+                                  Resource = "/data/contact/" + contact.id,
+                                  RequestFormat = DataFormat.Json
+                              };
+            request.AddBody(contact);
+
+            var response = _client.Execute<Contact>(request);
+            return response.Data;
+        }
+
+        public HttpStatusCode DeleteContact(int id)
+        {
+            var request = new RestRequest(Method.DELETE)
+                              {
+                                  Resource = "/data/contact/" + id
+                              };
+            var response = _client.Execute<Contact>(request);
+            return response.StatusCode;
+        }
+
+
+        #endregion
+    }
+}
---
+++ contact-helper/ContactSample/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ContactSample.Models
+{
+    public class Contact
+    {
+        public string accountName { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string address3 { get; set; }
+        public string businessPhone { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string emailAddress { get; set; }
+        public string firstName { get; set; }
+        public int? id { get; set; }
+        public string lastName { get; set; }
+        public bool? isSubscribed { get; set; }
+        public bool? isBounceBack { get; set; }
+        public string salesPerson { get; set; }
+        public string title { get; set; }
+        public List<FieldValue> fieldValues { get; set; }
+    }
+}
