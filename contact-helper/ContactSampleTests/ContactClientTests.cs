e18026aabd10e4f47b79a303e5c41c7b4b7a0562
Fred Sakr <fred.sakr@gmail.com>
2013-08-19 08:05:11 -0400

    changing helper classes to clients - updating contact contract object

---
+++ contact-helper/ContactSampleTests/ContactClientTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using ContactSample;
+using ContactSample.Models;
+using NUnit.Framework;
+
+namespace ContactSampleTests
+{
+    [TestFixture]
+    public class ContactClientTests
+    {
+        #region properties
+
+        private ContactClient _client;
+
+        #endregion
+
+        #region setup / teardown
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            _client = new ContactClient("site", "user", "password", "https://secure.eloqua.com/API/REST/2.0");
+        }
+
+        #endregion
+
+        #region tests
+
+        [Test]
+        public void GetContactTest()
+        {
+            int id = 1;
+            var contact = _client.GetContact(id);
+            Assert.AreEqual(contact.id, id);
+        }
+
+        [Test]
+        public void SearchContactsTest()
+        {
+            var contacts = _client.SearchContacts("*", 1, 10);
+            Assert.Greater(contacts.total, 0);
+        }
+
+        [Test]
+        public void CreateContactTest()
+        {
+            Contact contact = new Contact()
+                                  {
+                                      emailAddress = "sample@test.com",
+                                      firstName = "Joe",
+                                      lastName = "Smith",
+                                      accountName = "Eloqua Corporation",
+                                      businessPhone = "123-456-7890"
+                                  };
+
+            var createdContact = _client.CreateContact(contact);
+            Assert.Greater(createdContact.id, 0);
+            Assert.AreEqual(contact.emailAddress, createdContact.emailAddress);
+        }
+
+        [Test]
+        public void DeleteContactTest()
+        {
+            int id = 1;
+            var statusCode = _client.DeleteContact(id);
+            Assert.AreEqual(HttpStatusCode.OK, statusCode);
+        }
+
+        #endregion
+
+    }
+}
